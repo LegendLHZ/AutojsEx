8d8d3cb2f688af52fe3d27fc820d22d95144de57
LHZ <1083400646@qq.com>
2024-05-24 18:09:58 +0800

    1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -416,7 +416,7 @@ pluginManagement {
 
         dependencies /* Android/Kotlin Gradle Plugin. */  {
             classpath("com.android.tools.build:gradle:8.3.2")
-            classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
+            classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
         }
 
     }
